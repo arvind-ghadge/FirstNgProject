b34e4a29062f40a27e15eb87b0b116e285d266dd
Arvindg-ESG\Arvind <arvindg@esolutionsgroup.co.uk>
2019-08-27 19:27:40 +0530

    add property review section

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -12,6 +12,16 @@ export class DetailsComponent implements OnInit {
   ngOnInit() {
   }
 
-  
+  propertyDetail:any = [
+    {name:"Condo for Rent in City Center"},
+    {address:"Property Detail"},
+    {tag:["Condo","6 Beds","6 Baths","3 Garages","1100 SqFt"]},
+    {propDetail:[
+      {propertyID:"HI-IW-CO-88",propertyType:"Condo", propertyStatus:"For Rent"}
+    ]},
+    {addDetail:[
+      {propertyID:"HI-IW-CO-88",propertyType:"Condo", propertyStatus:"For Rent"}
+    ]}
+  ]
 
 }
