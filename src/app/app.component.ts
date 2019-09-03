29e5457994e15065549f1063ac32f5ab6f6f2a02
Arvindg-ESG\Arvind <arvindg@esolutionsgroup.co.uk>
2019-09-03 19:13:23 +0530

    new service add and add variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { NewserviceService } from './newservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [NewserviceService]
 })
 export class AppComponent implements OnInit {
   title = 'app works';
   myTitle:any;
-  constructor(){
+  text:any;
+  constructor( private _newservice:NewserviceService ){
     
+  }
+
+  ngOnInit(){
+  
+    this.text=this._newservice.display();
   }
 
   outputMethod(){
     this.title = "My App";
   }
   
-  ngOnInit(){
   
-    
-  }
   
   
 
