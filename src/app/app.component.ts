0561bc1ff15d2a676cd5efb0feb2a4f794f60aef
Arvindg-ESG\Arvind <arvindg@esolutionsgroup.co.uk>
2019-09-19 17:26:21 +0530

    student list service add and add the tables of students information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { NewserviceService } from './newservice.service';
 import { HeroListService } from './services/herolist.service';
 import { ContentListService } from './new-services/contentlist.service';
+import { StudentListService } from './services/studentslist.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [NewserviceService, HeroListService, ContentListService]
+  providers: [NewserviceService, HeroListService, ContentListService, StudentListService]
 })
 export class AppComponent implements OnInit {
   title = 'app works';
   myTitle:any;
   text:any;
+  public students: any = [];
   heroes;
   contentList;
-  constructor( private _newservice:NewserviceService, private herolistService:HeroListService, private contentlistService:ContentListService ){
+  constructor( private _newservice:NewserviceService, 
+    private herolistService:HeroListService, 
+    private contentlistService:ContentListService,
+    private _studentListService:StudentListService ){
     this.heroes = this.herolistService.getAllStudents();
     this.contentList = this.contentlistService.getData();
+    this.students = this._studentListService.getAllStudents();
   }
   
-
-  ngOnInit(){
   
+  ngOnInit(){
+    this.students = this._studentListService.getAllStudents();
     this.text=this._newservice.display();
   }
 
---
+++ src/app/services/studentslist.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class StudentListService{
+    getAllStudents(){
+        return[{
+            id: 1,
+            Name: "Mahesh",
+            Address: "Thane",
+            Gender: "Male"
+        },
+        {
+            id: 2,
+            Name: "Arvind",
+            Address: "Thane",
+            Gender: "Male"
+        },
+        {
+            id: 3,
+            Name: "Manisha",
+            Address: "Vashi",
+            Gender: "Female"
+        },
+        {
+            id: 4,
+            Name: "Aabid",
+            Address: "Koparkhairane",
+            Gender: "Male"
+        },
+        {
+            id: 5,
+            Name: "Vanaja",
+            Address: "Bhiwandi",
+            Gender: "Female"
+        }
+    ];
+    }
+}
